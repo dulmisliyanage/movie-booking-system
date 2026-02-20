c9a362ba86258150c6dcce54dc1d9d21f0e15a2a
Dulmi Sawindi <dulmi.s.liyanage@gmail.com>
2026-02-21 01:28:05 +0530

    Update API URL to production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://movie-booking-system-dv54.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
